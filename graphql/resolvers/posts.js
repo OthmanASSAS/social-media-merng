764ccfe234806e2b1f3031478b23caf41a6bcf98
manas <othman_assas@hotmail.fr>
2021-04-28 08:55:18 +0200

    delete posts with check user

--- graphql/resolvers/posts.js
+++ graphql/resolvers/posts.js
@@ -1,3 +1,4 @@
+const { AuthenticationError } = require('apollo-server');
 const Post = require('../../models/Post');
 
 const checkAuth = require('../../utils/check-auth');
@@ -6,7 +7,7 @@ module.exports = {
   Query: {
     async getPosts() {
       try {
-        const posts = await Post.find();
+        const posts = await Post.find().sort({ createdAt: -1 });
         return posts;
       } catch (err) {
         throw new Error(err);
@@ -28,7 +29,6 @@ module.exports = {
   Mutation: {
     async createPost(_, { body }, context) {
       const user = checkAuth(context);
-      console.log({ user });
       const newPost = new Post({
         body,
         user: user.id,
@@ -37,6 +37,21 @@ module.exports = {
       });
       const post = await newPost.save();
       return post;
+    },
+
+    async deletePost(_, { postId }, context) {
+      const user = checkAuth(context);
+      try {
+        const post = await Post.findById(postId);
+        if (user.username === post.username) {
+          await post.delete();
+          return 'Post deleted successfully';
+        } else {
+          throw new AuthenticationError('Action not allowed');
+        }
+      } catch (err) {
+        throw new Error(err);
+      }
     },
   },
 };
--- utils/check-auth.js
+++ utils/check-auth.js
@@ -6,13 +6,10 @@ const { SECRET_KEY } = require('../config');
 module.exports = (context) => {
   const authHeader = context.req.headers.authorization;
   if (authHeader) {
-    console.log('*** authHeader ***', authHeader);
     const token = authHeader.split('Bearer ')[1];
-    console.log('*** TOKEN ***', token);
     if (token) {
       try {
         const user = jwt.verify(token, SECRET_KEY);
-        console.log('***USER***', user);
         return user;
       } catch (err) {
         throw new AuthenticationError('Invalid/expired token');
